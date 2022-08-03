09b3d974e257bf567928276f783db3b7778e5d48
Yorí <yorigcsdev@gmail.com>
2022-08-03 19:59:56 -0300

    feat: add privateRoutes and publicRoutes to server.use

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+import privateRoutes from './routes/privateRoutes.js'
+import publicRoutes from './routes/publicRoutes.js'
+
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const PORT = process.env.PORT || 5000
 const server = express()
 
-server.use(express.json())
+server.use([express.json(), cors(), privateRoutes, publicRoutes])
 
 server.listen(PORT, () => console.log('listening on port ' + PORT))

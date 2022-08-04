ba3fbbc9271df6ba63a7839c5604dc584426d8c3
Yorí <yorigcsdev@gmail.com>
2022-08-04 17:07:57 -0300

    feat: create getMyUrlsMiddleware

---
+++ src/middlewares/getMyUrlsMiddleware.js
@@ -0,0 +1,13 @@
+
+import { decodeToken } from '../services/jwtToken.js'
+
+const getMtUrlsMiddleware = (req, res, next) => {
+  const TOKEN = req.header('Authorization')?.split('Bearer ')[1]
+  const decoded = decodeToken(TOKEN)
+  if (!decoded) {
+    return res.status(401).send('Token Inválido')
+  }
+  res.locals.myId = decoded.id
+  next()
+}
+export default getMtUrlsMiddleware

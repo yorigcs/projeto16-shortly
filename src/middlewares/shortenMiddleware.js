76f7575dde1364f7a289b1aa6042466b1fdb7c99
Yorí <yorigcsdev@gmail.com>
2022-08-04 13:46:29 -0300

    refactor: change jwt decode in shortenMiddleware

--- src/middlewares/shortenMiddleware.js
+++ src/middlewares/shortenMiddleware.js
@@ -1,19 +1,16 @@
 import shortenURLSchema from '../schemas/shortenURLSchema.js'
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
-dotenv.config()
-const SECRET = process.env.SECRET_KEY || '$1AIKSO%6A41'
-const shortenMiddleware = async (req, res) => {
+import { decodeToken } from '../services/jwtToken.js'
+const shortenMiddleware = async (req, res, next) => {
   const { url } = req.body
   const TOKEN = req.header('Authorization')?.split('Bearer ')[1]
   try {
     await shortenURLSchema.validateAsync({ url })
-    try {
-      const decoded = jwt.verify(TOKEN, SECRET)
-      console.log(decoded)
-    } catch (err) {
+    const decoded = decodeToken(TOKEN)
+    if (!decoded) {
       return res.status(401).send('Token Inválido')
     }
+    res.locals.shortenData = { userId: decoded.id, url }
+    next()
   } catch (err) {
     const { message } = err.details[0]
     console.log(message)

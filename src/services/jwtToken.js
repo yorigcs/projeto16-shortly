45165566a3296206a557ec872dbcd9d5259bdedc
Yorí <yorigcsdev@gmail.com>
2022-08-04 13:20:21 -0300

    feat: create service jwToken to handle token creation and decode token

---
+++ src/services/jwtToken.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const SECRET = process.env.SECRET_KEY || '$1AIKSO%6A41'
+const EXPIRED_TIME = process.env.TOKEN_EXP_TIME || '1h'
+
+const createToken = (payload) => {
+  return jwt.sign(payload, SECRET, EXPIRED_TIME)
+}
+
+const decodeToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, SECRET)
+    return decoded
+  } catch (err) {
+    return null
+  }
+}
+
+export { createToken, decodeToken }

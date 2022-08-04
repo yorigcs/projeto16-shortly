d18ae5470d387796929a7f0913ec292f129c8419
Yorí <yorigcsdev@gmail.com>
2022-08-04 20:50:45 -0300

    feat: create getRanksController

---
+++ src/controllers/getRanksController.js
@@ -0,0 +1,21 @@
+import connection from '../database/postgressSQL'
+
+const getRanksController = async (req, res) => {
+  const query = `
+  SELECT u.id, u.name, COUNT(l.views) AS "linksCount", COALESCE(SUM(l.views),0) AS "visitCount"
+  FROM users u 
+  LEFT JOIN 
+  links l 
+  ON u.id = l.user_id
+  GROUP BY u.id 
+  ORDER BY "visitCount" DESC 
+  LIMIT(10)`
+
+  try {
+    const { rows: rank } = await connection.query(query)
+    res.send(rank)
+  } catch (err) {
+    res.sendStatus(500)
+  }
+}
+export default getRanksController

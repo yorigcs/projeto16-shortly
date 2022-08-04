45e71f18c53403645db6f164f6b73f05bd418a5b
Yorí <yorigcsdev@gmail.com>
2022-08-04 17:58:47 -0300

    feat: add getMyUrlsMiddleware and getMyUrlsController to privateRoutes /users/me

--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -3,11 +3,13 @@ import deleteUrlController from '../controllers/deleteUrlController.js'
 import shortenURLController from '../controllers/shortenURLController.js'
 import deleteShortenUrlMiddleware from '../middlewares/deleteUrlMiddleware.js'
 import shortenMiddleware from '../middlewares/shortenMiddleware.js'
+import getMyUrlsMiddleware from '../middlewares/getMyUrlsMiddleware.js'
+import getMyUrlsController from '../controllers/getMyUrlsController.js'
 
 const router = Router()
 
 router.post('/urls/shorten', shortenMiddleware, shortenURLController)
 router.delete('/urls/:id', deleteShortenUrlMiddleware, deleteUrlController)
-router.get('/users/me')
+router.get('/users/me', getMyUrlsMiddleware, getMyUrlsController)
 
 export default router
